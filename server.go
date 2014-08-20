1d4d7dee3149727a74eacb988d23b1802b3682c0
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2014-08-20 14:28:40 +0900

    parse win packet

--- server.go
+++ server.go
@@ -25,7 +25,7 @@ func receivePayloadProcess(payloadChannel chan *payload.Payload,
 		currentPayload.Conn = udpConn
 		currentPayload.Buffer = buffer
 		currentPayload.BufferLength = bufferLength
-		//log.Println("receivePayload currentPayload:", currentPayload.Buffer[0])
+		//log.Println("receivePayload currentPayload:", bufferLength)
 
 		payloadChannel <- currentPayload
 	}
@@ -33,11 +33,91 @@ func receivePayloadProcess(payloadChannel chan *payload.Payload,
 	return nil
 }
 
+func IntToBcd(value int) int {
+	bcd := (((value / 10) % 10) << 4) | (value % 10)
+	return bcd
+}
+
+func bcdToInt(value int) int {
+	return (int)((value>>4)*10 + (value & 0x0F))
+}
+
 func processPayload(payloadChannel chan *payload.Payload) error {
 	for {
 		currentPayload := <-payloadChannel
 		//log.Println("processPayload currentPayload:", currentPayload)
-		log.Println("currentPayload.buffer:%04hX ", currentPayload.Buffer[0:1])
+		//log.Println("currentPayload.buffer:%04hX ", currentPayload.Buffer[0:1])
+		seq := currentPayload.Buffer[0:2]
+		A0 := currentPayload.Buffer[2:3]
+		length := currentPayload.Buffer[3:5]
+		bcddate := currentPayload.Buffer[5:11]
+
+		year := bcdToInt(int(bcddate[0]))
+		month := bcdToInt(int(bcddate[1]))
+		day := bcdToInt(int(bcddate[2]))
+		hour := bcdToInt(int(bcddate[3]))
+		minute := bcdToInt(int(bcddate[4]))
+		second := bcdToInt(int(bcddate[5]))
+
+		ch := currentPayload.Buffer[11:13]
+		size := currentPayload.Buffer[13] >> 4
+		rate := (currentPayload.Buffer[13]&0x0f)<<8 | currentPayload.Buffer[14]&0xff
+
+		firstSample := currentPayload.Buffer[15:19]
+
+		startPos := 19
+
+		datetime := fmt.Sprintf("%02d%02d%02d%02d%02d%02d", year, month, day, hour, minute, second)
+
+		fmt.Printf("%04X %X %X %s %04X %d %d %08X\n", seq, A0, length, datetime, ch, size, rate, firstSample)
+
+		if size == 0 {
+			rate = rate / 2
+
+		}
+
+		for i := 0; i < int(rate)-1; i++ {
+			if size == 4 {
+				s := int(startPos + (i * 4))
+				e := int(startPos + (i * 4) + 4)
+
+				diff := currentPayload.Buffer[s:e]
+				fmt.Printf("s:%d e:%d index:%d %04X\n", s-19, e-19, i, diff)
+			}
+			if size == 3 {
+				s := int(startPos + (i * 3))
+				e := int(startPos + (i * 3) + 3)
+
+				diff := currentPayload.Buffer[s:e]
+				fmt.Printf("s:%d e:%d index:%d %04X\n", s-19, e-19, i, diff)
+			}
+
+			if size == 2 {
+				s := int(startPos + (i * 2))
+				e := int(startPos + (i * 2) + 2)
+
+				diff := currentPayload.Buffer[s:e]
+				fmt.Printf("s:%d e:%d index:%d %04X\n", s-19, e-19, i, diff)
+			}
+
+			if size == 1 {
+				s := int(startPos + (i * 1))
+				e := int(startPos + (i * 1) + 1)
+
+				diff := currentPayload.Buffer[s:e]
+				fmt.Printf("s:%d e:%d index:%d %X\n", s-19, e-19, i, diff)
+			}
+
+			if size == 0 {
+				s := int(startPos + (i * 1))
+				e := int(startPos + (i * 1) + 1)
+
+				diff := currentPayload.Buffer[s:e]
+				fmt.Printf("s:%d e:%d index:%d %X\n", s-19, e-19, i, diff)
+
+			}
+		}
+
 	}
 
 	return nil
